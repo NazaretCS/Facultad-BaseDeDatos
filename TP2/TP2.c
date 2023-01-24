7ad10200c1275b17d68f1b680ec1ab6c1b2f1264
NazaretCS <nazaretcampos27@gmail.com>
2023-01-24 11:26:55 -0300

    Trabajo Práctico Número 2

---
+++ TP2/TP2.c
@@ -0,0 +1,72 @@
+/**
+
+
+Entidad:    Hotel
+
+Atributos:     CUIL
+               Nombre
+               Direccion 
+               Telefono,
+               Año de Construccion
+               PK
+               Tipos de habitaciones 
+
+Entidad:    Categoria
+
+Atributos:      Calificacion, año, PK, Servicios que debe brinda, Comodidades que debe brindar. 
+
+
+Entidad:    Habitación 
+
+Atributos:      Numero de Habitacion
+                Codigo
+                Numero
+                El tipo
+                Precio 
+                Metros cuadrados que ocupa.
+
+
+
+Entidad:    Reserva
+
+Atributos:      
+
+
+
+Punto 1 c
+
+    Entidad:    Clientes.
+    Atributos:      Nombre completo
+                    Direccion
+                    Telefonos.
+                    Codigo asignado por la empresa
+
+
+
+    Entidad:    Pedido.
+    Atributos       Punto de Partida.
+                    Punto de entrega.
+                    Fecha en que se realizo el pedido
+                    Hora en que se realizo el pedido
+                    costo del viaje.
+                    Numero de Pedido.
+                    
+
+
+
+    Entidad:    Cadetes de la empresa
+    Atrinutos:      Nombre Completo.
+                    Direccion
+                    Telefonos.
+                    DNI
+                    Fecha de Nacimiento.
+                    Tipo de Vehiculo con el que realizan su trabajo
+                    Codigo asignado por la empresa
+
+
+
+    Entidad:    Pago Semanal.
+    Atributos:      Monto.
+                    Fecha de Pago
+                    Numero de Semana.
+                    Numero de Pago.
